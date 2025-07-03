877ee3ed29f7fae29d9002f0e1cbaabed780a0c0
Smarttravelly.com <smarttravelly@gmail.com>
2025-07-03 16:00:23 +0700

    Update urls.js

--- urls.js
+++ urls.js
@@ -1,4 +1,4 @@
 window.urlMap = {
-  // Example: 'sadgjkfhaskljgfhkl': 'https://example.com'
-  // Add new mappings here manually
+  'sadgjkfhaskljgfhkl': 'https://www.youtube.com/watch?v=KO0v00yt3zY&list=RDKO0v00yt3zY&start_radio=1'
+  // Add more mappings here manually
 };
